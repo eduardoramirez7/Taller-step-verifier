09f3fcdb6c000f5ea43c826037de489bfbca1e6e
Luisa Ibagon <jeduard@unicauca.edu.co>
2021-06-27 22:29:34 -0500

    Ejercicios stepverifier y testpublisher

---
+++ src/test/java/com/sofka/pruebasunitarias/PruebasUnitariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.pruebasunitarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasUnitariasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/servicios/ServicioTest.java
@@ -0,0 +1,50 @@
+package servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@SpringBootTest
+class ServicioTest {
+
+    @Autowired
+    Servicio servicio;
+    @Test
+    void testMono() {
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+    @Test
+    void testVarios() {
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+
+    @Test
+    void testTodosFiltro() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .create(source)
+                .expectNextCount(4)
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
+                        throwable.getMessage().equals("Mensaje de Error")
+                ).verify();
+    }
+
+    @Test
+    void testBasadoEnTiempo() {
+        Flux<Integer> source = servicio.publicadorBasadoEnTiempo();
+        StepVerifier.create(source)
+                .expectNext(2)
+                .expectComplete()
+                .verifyThenAssertThat()
+                .hasDropped(4)
+                .tookLessThan(Duration.ofMillis(1050));
+    }
+
+}
